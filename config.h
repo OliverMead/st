e0d20c8e9d675bec940abdaae17798bd4554ea69
Oliver Mead <oliver.j.mead@protonmail.com>
2022-03-12 02:23:30 +0000

    fira code ligatures support ==> greatness

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:bold:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Fira Code Nerd Font:medium:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
